879f600a934239be86649681bdd0280d8a421b66
Zhenkun Guo <1608229093@qq.com>
2021-11-28 22:24:15 +0800

    for循环变种

---
+++ For-Variant.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int x, y;
+	for (x = 0, y = 0; x < 2 && x < 5; ++x, y++)
+	{
+		printf("66\n");
+
+	}
+	return 0;
+}
+
+int main()
+{
+	
+	int i = 0;
+	int j = 0;
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			printf("666\n");
+		}
+	}
+		
+	/*int i = 0;
+	int j = 0;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("666\n");
+
+		}
+	}*/
+	return 0;
+}
